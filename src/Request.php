fef4913c91061ea408e77781e000d43c6daf0f8e
Paul Water <p.r.water@setbased.nl>
2024-09-29 10:55:12 +0200

    Redesign interface.

--- src/Request.php
+++ src/Request.php
@@ -5,23 +5,59 @@ namespace Plaisio\Request;
 
 /**
  * Interface providing information about an HTTP request.
+ *
+ * @property-read string  $absoluteUrl        The requested absolute URL.
+ * @property-read string  $acceptContentType  The content types acceptable by the user agent as sent by the user agent.
+ * @property-read array   $acceptContentTypes The content types acceptable by the user agent ordered by the
+ *                                            corresponding quality scores.
+ * @property-read string  $acceptEncoding     The acceptable encodings by the user agent as sent by the user agent.
+ * @property-read array   $acceptEncodings    The acceptable encodings by the user agent sorted by the corresponding
+ *                                            quality scores.
+ * @property-read string  $acceptLanguage     The languages acceptable by the user agent as sent by the user agent.
+ * @property-read array   $acceptLanguages    The languages acceptable by the user agent sorted by preference. The first
+ *                                            element is the most preferred language.
+ * @property-read string  $baseUrl            The requested relative URL.
+ * @property-read ?string $contentType        The content type of the send media type.
+ * @property-read bool    $isAjax             Whether this is an AJAX (XMLHttpRequest) request.
+ * @property-read bool    $isDelete           Whether this is a DELETE request.
+ * @property-read bool    $isEnvDev           Whether the current environment is a development environment.
+ * @property-read bool    $isEnvProd          Whether the current environment is a production environment.
+ * @property-read bool    $isGet              Whether this is a GET request.
+ * @property-read bool    $isHead             Whether this is a HEAD request.
+ * @property-read bool    $isOptions          Whether this is a OPTIONS request.
+ * @property-read bool    $isPatch            Whether this is a PATCH request.
+ * @property-read bool    $isPost             Whether this is a POST request.
+ * @property-read bool    $isPut              Whether this is a PUT request.
+ * @property-read bool    $isSecureChannel    Whether the request is sent via a secure channel (https).
+ * @property-read string  $hostname           The hostname of the requested URL.
+ * @property-read string  $method             The method of the request.
+ * @property-read int     $port               The port to which the request was made.
+ * @property-read ?string $referrer           The URL of the page (if any) which referred the user agent to the current
+ *                                            page. This is set by the user agent and cannot be trusted.
+ * @property-read ?string $remoteIp           The remote IP (this is always the next hop, not necessarily the user's IP
+ *                                            address).
+ * @property-read ?float  $requestTime        The timestamp of the start of the request, with microsecond precision.
+ * @property-read string  $requestUri         The requested relative URL after. It includes the query part if any.
+ * @property-read array   $secureHeaders      The lists of headers that are, by default, subject to the trusted host
+ *                                            configuration.
+ * @property-read ?string $userAgent          The user agent.
  */
 interface Request
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Returns the accepted languages by the user agent.
-   *
-   * @return string|null
-   *
-   * @api
-   * @since 1.0.0
+   * The default port for unencrypted connections.
    */
-  public function getAcceptLanguage(): ?string;
+  public const int HTTP_PORT = 80;
+
+  /**
+   * The default port for encrypted connections.
+   */
+  public const int HTTPS_PORT = 443;
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Returns the value of cookie sent by the user agent.
+   * Returns the value of a cookie sent by the user agent.
    *
    * @param string $name The name of the cookie.
    *
@@ -47,17 +83,6 @@ interface Request
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Returns the method of the current request.
-   *
-   * @return string
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function getMethod(): string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
    * Returns the value of an optional HTTP header sent by the user agent.
    *
    * @param string $header The name of the HTTP header (case-insensitive and without leading HTTP_).
@@ -68,177 +93,6 @@ interface Request
    * @since 1.0.0
    */
   public function getOptHeader(string $header): ?string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns the URL of the page (if any) which referred the user agent to the current page. This is set by the user
-   * agent and cannot be trusted.
-   *
-   * @return string|null
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function getReferrer(): ?string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns the remote IP (this is always the next hop, not necessarily the user's IP address).
-   *
-   * @return string|null
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function getRemoteIp(): ?string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns the timestamp of the start of the request, with microsecond precision.
-   *
-   * @return float|null
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function getRequestTime(): ?float;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns the requested relative URL after. It includes the query part if any.
-   *
-   * @return string
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function getRequestUri(): string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns the user agent.
-   *
-   * @return string|null
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function getUserAgent(): ?string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is an AJAX (XMLHttpRequest) request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isAjax(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a DELETE request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isDelete(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether the current environment is a development environment.
-   *
-   * @return bool
-   */
-  public function isEnvDev(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether the current environment is a production environment.
-   *
-   * @return bool
-   */
-  public function isEnvProd(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a GET request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isGet(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a HEAD request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isHead(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a OPTIONS request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isOptions(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a PATCH request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isPatch(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a POST request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isPost(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether this is a PUT request.
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isPut(): bool;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Returns whether the request is sent via a secure channel (https).
-   *
-   * @return bool
-   *
-   * @api
-   * @since 1.0.0
-   */
-  public function isSecureChannel(): bool;
 
   //--------------------------------------------------------------------------------------------------------------------
 }
