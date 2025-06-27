230cab76abfe14d03f7a40fb0321b71d5e5c39fc
Paul Water <p.r.water@setbased.nl>
2025-06-27 10:40:08 +0200

    Add property $cgi.

--- src/Request.php
+++ src/Request.php
@@ -6,41 +6,43 @@ namespace Plaisio\Request;
 /**
  * Interface providing information about an HTTP request.
  *
- * @property-read string  $absoluteUrl        The requested absolute URL.
- * @property-read string  $acceptContentType  The content types acceptable by the user agent as sent by the user agent.
- * @property-read array   $acceptContentTypes The content types acceptable by the user agent ordered by the
- *                                            corresponding quality scores.
- * @property-read string  $acceptEncoding     The acceptable encodings by the user agent as sent by the user agent.
- * @property-read array   $acceptEncodings    The acceptable encodings by the user agent sorted by the corresponding
- *                                            quality scores.
- * @property-read string  $acceptLanguage     The languages acceptable by the user agent as sent by the user agent.
- * @property-read array   $acceptLanguages    The languages acceptable by the user agent sorted by preference. The first
- *                                            element is the most preferred language.
- * @property-read string  $baseUrl            The requested relative URL.
- * @property-read ?string $contentType        The content type of the send media type.
- * @property-read bool    $isAjax             Whether this is an AJAX (XMLHttpRequest) request.
- * @property-read bool    $isDelete           Whether this is a DELETE request.
- * @property-read bool    $isEnvDev           Whether the current environment is a development environment.
- * @property-read bool    $isEnvProd          Whether the current environment is a production environment.
- * @property-read bool    $isGet              Whether this is a GET request.
- * @property-read bool    $isHead             Whether this is a HEAD request.
- * @property-read bool    $isOptions          Whether this is a OPTIONS request.
- * @property-read bool    $isPatch            Whether this is a PATCH request.
- * @property-read bool    $isPost             Whether this is a POST request.
- * @property-read bool    $isPut              Whether this is a PUT request.
- * @property-read bool    $isSecureChannel    Whether the request is sent via a secure channel (https).
- * @property-read string  $hostname           The hostname of the requested URL.
- * @property-read string  $method             The method of the request.
- * @property-read int     $port               The port to which the request was made.
- * @property-read ?string $referrer           The URL of the page (if any) which referred the user agent to the current
- *                                            page. This is set by the user agent and cannot be trusted.
- * @property-read ?string $remoteIp           The remote IP (this is always the next hop, not necessarily the user's IP
- *                                            address).
- * @property-read ?float  $requestTime        The timestamp of the start of the request, with microsecond precision.
- * @property-read string  $requestUri         The requested relative URL after. It includes the query part if any.
- * @property-read array   $secureHeaders      The lists of headers that are, by default, subject to the trusted host
- *                                            configuration.
- * @property-read ?string $userAgent          The user agent.
+ * @property-read string  $absoluteUrl          The requested absolute URL.
+ * @property-read string  $acceptContentType    The content types acceptable by the user agent as sent by the user
+ *                agent.
+ * @property-read array   $acceptContentTypes   The content types acceptable by the user agent ordered by the
+ *                                              corresponding quality scores.
+ * @property-read string  $acceptEncoding       The acceptable encodings by the user agent as sent by the user agent.
+ * @property-read array   $acceptEncodings      The acceptable encodings by the user agent sorted by the corresponding
+ *                                              quality scores.
+ * @property-read string  $acceptLanguage       The languages acceptable by the user agent as sent by the user agent.
+ * @property-read array   $acceptLanguages      The languages acceptable by the user agent sorted by preference. The
+ *                                              first element is the most preferred language.
+ * @property-read string  $baseUrl              The requested relative URL.
+ * @property-read array   $cgi                  The CGI parameters.
+ * @property-read ?string $contentType          The content type of the send media type.
+ * @property-read bool    $isAjax               Whether this is an AJAX (XMLHttpRequest) request.
+ * @property-read bool    $isDelete             Whether this is a DELETE request.
+ * @property-read bool    $isEnvDev             Whether the current environment is a development environment.
+ * @property-read bool    $isEnvProd            Whether the current environment is a production environment.
+ * @property-read bool    $isGet                Whether this is a GET request.
+ * @property-read bool    $isHead               Whether this is a HEAD request.
+ * @property-read bool    $isOptions            Whether this is an OPTIONS request.
+ * @property-read bool    $isPatch              Whether this is a PATCH request.
+ * @property-read bool    $isPost               Whether this is a POST request.
+ * @property-read bool    $isPut                Whether this is a PUT request.
+ * @property-read bool    $isSecureChannel      Whether the request is sent via a secure channel (https).
+ * @property-read string  $hostname             The hostname of the requested URL.
+ * @property-read string  $method               The method of the request.
+ * @property-read int     $port                 The port to which the request was made.
+ * @property-read ?string $referrer             The URL of the page (if any) which referred the user agent to the
+ *                                              current page. This is set by the user agent and cannot be trusted.
+ * @property-read ?string $remoteIp             The remote IP (this is always the next hop, not necessarily the user's
+ *                                              IP address).
+ * @property-read ?float  $requestTime          The timestamp of the start of the request, with microsecond precision.
+ * @property-read string  $requestUri           The requested relative URL after. It includes the query part if any.
+ * @property-read array   $secureHeaders        The lists of headers that are, by default, subject to the trusted host
+ *                                              configuration.
+ * @property-read ?string $userAgent            The user agent.
  */
 interface Request
 {
@@ -96,7 +98,7 @@ interface Request
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Validates the request headers. A BadRequestException must be thrown if the request headers are invalid.
+   * Validates the request. A BadRequestException must be thrown if the request is invalid.
    */
   public function validate(): void;
 
